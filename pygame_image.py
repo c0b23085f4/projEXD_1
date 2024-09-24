827c946fcb26251292c6def8b46e9bbf6d9ce6d2
takeru <c0b23085f4@edu.teu.ac.jp>
2024-09-24 15:28:32 +0900

    こうかとんの左右反転

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,12 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    # 練習2
+    kk_img = pg.image.load("fig/3.jpg")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 10.0)
+
+
     tmr = 0
     while True:
         for event in pg.event.get():
