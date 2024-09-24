ff3cb27c274c1ade4dc1eae4b1827990f4057cad
takeru <c0b23085f4@edu.teu.ac.jp>
2024-09-24 16:42:39 +0900

    演習課題

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -24,15 +24,18 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        mx, my = 0, 0
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            mx, my = 0, -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
+            mx, my= 0, +1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            mx, my= -1, 0
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            mx, my = +2, 0
+
+        kk_rct.move_ip(mx, my)
 
         
 
@@ -42,10 +45,12 @@ def main():
         screen.blit(bg_filp_img,[x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_filp_img,[x+4800, 0])
+        curr_x, curr_y = kk_rct.center
+        kk_rct.center = curr_x - 1, curr_y
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(2000)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
